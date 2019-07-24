9d9cf062cf8dbe9d85ecc773b065953316bcdd5f
shaily <shaily@github.com>
2019-07-24 15:33:17 +0530

    Beautiful soup refactored using python

--- bs4.py
+++ bs4.py
@@ -1,21 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-page_number = raw_input('Enter a page number: ')
+page_number = raw_input('Enter a page number to retreive information from: ')
 r = requests.get("https://www.swiggy.com/udaipur?page="+str(page_number))
 soup = BeautifulSoup(r.content)
 name = []
 cuisine = []
 restaurant_information = []
 for item in soup.find_all('div',{'class': 'nA6kb'}):
-	name.append(item.text)
+  name.append(item.text)
 for item in soup.find_all('div',{'class': '_1gURR'}):
-	item_info = item.text
-	cuisine.append(item_info)
+  item_info = item.text
+  cuisine.append(item_info)
 name_info = [str(i) for i in name]
 cuisine_info = [str(i) for i in cuisine]
 for restaurant_name,cusinie_type in zip(name_info, cuisine_info):
-    restaurant_information.append([restaurant_name, cusinie_type])
-print restaurant_information
-
-
+  restaurant_information.append([restaurant_name, cusinie_type])
+restaurant_information
